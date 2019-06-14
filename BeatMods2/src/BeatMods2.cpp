346004791e054e066d2749727bc1bead13786e28
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-06-14 17:50:34 -0500

    Initial commit

---
+++ BeatMods2/src/BeatMods2.cpp
@@ -0,0 +1,12 @@
+﻿// BeatMods2.cpp : Defines the entry point for the application.
+//
+
+#include "BeatMods2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
