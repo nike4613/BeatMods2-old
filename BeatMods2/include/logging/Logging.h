d6104f7e91ee4c155e068ba9167052e2026ee117
Anairkoen Schno <aaron.cirr.com@gmail.com>
2019-06-15 15:19:32 -0500

    Fixed VS doing garbage tabs

--- BeatMods2/include/logging/Logging.h
+++ BeatMods2/include/logging/Logging.h
@@ -9,40 +9,40 @@
 #define LOGGER_BUFFER_SIZE 2048
 
 namespace std {
-	std::string to_string(const restbed::Logger::Level& level);
+    std::string to_string(const restbed::Logger::Level& level);
 }
 
 namespace BeatMods {
-	class UnifiedLogger : public restbed::Logger {
-	public:
-		void start(const std::shared_ptr<const restbed::Settings>& settings) override;
-		void stop() override; // provides empty defaults
-		void log(const Level level, const char* format, ...) override;
-		void log_if(bool expression, const Level level, const char* format, ...) override;
+    class UnifiedLogger : public restbed::Logger {
+    public:
+        void start(const std::shared_ptr<const restbed::Settings>& settings) override;
+        void stop() override; // provides empty defaults
+        void log(const Level level, const char* format, ...) override;
+        void log_if(bool expression, const Level level, const char* format, ...) override;
 
-		virtual void write(const Level level, std::string_view) = 0;
-	};
+        virtual void write(const Level level, std::string_view) = 0;
+    };
 
-	class StdoutLogger : public UnifiedLogger {
-	public:
-		void write(const Level level, std::string_view) override;
-		static std::shared_ptr<StdoutLogger> New();
-	};
+    class StdoutLogger : public UnifiedLogger {
+    public:
+        void write(const Level level, std::string_view) override;
+        static std::shared_ptr<StdoutLogger> New();
+    };
 
-	class MultiLogger : public UnifiedLogger {
-		std::vector<std::shared_ptr<UnifiedLogger>> loggers;
+    class MultiLogger : public UnifiedLogger {
+        std::vector<std::shared_ptr<UnifiedLogger>> loggers;
 
-		MultiLogger() = default;
-		MultiLogger(std::initializer_list<std::shared_ptr<UnifiedLogger>>&& loggers) : loggers(loggers) {}
-	public:
-		void write(const Level level, std::string_view) override;
+        MultiLogger() = default;
+        MultiLogger(std::initializer_list<std::shared_ptr<UnifiedLogger>>&& loggers) : loggers(loggers) {}
+    public:
+        void write(const Level level, std::string_view) override;
 
-		template<typename...T> MultiLogger(const std::shared_ptr<T>&... Args) 
-			: MultiLogger( { (static_cast<std::shared_ptr<UnifiedLogger>>(Args), ...) } ) {}
+        template<typename...T> MultiLogger(const std::shared_ptr<T>&... Args) 
+            : MultiLogger( { (static_cast<std::shared_ptr<UnifiedLogger>>(Args), ...) } ) {}
 
-		template<typename...T> static std::shared_ptr<MultiLogger> New(const std::shared_ptr<T>&... Args) 
-			{ return std::make_shared<MultiLogger>(Args...); }
-	};
+        template<typename...T> static std::shared_ptr<MultiLogger> New(const std::shared_ptr<T>&... Args) 
+            { return std::make_shared<MultiLogger>(Args...); }
+    };
 }
 
 #endif
--- BeatMods2/src/BeatMods2.cpp
+++ BeatMods2/src/BeatMods2.cpp
@@ -11,33 +11,33 @@
 #include "logging/Logging.h"
 
 namespace {
-	void GET_root_handler(const std::shared_ptr<restbed::Session> session)
-	{
-		const auto& req = session->get_request();
+    void GET_root_handler(const std::shared_ptr<restbed::Session> session)
+    {
+        const auto& req = session->get_request();
 
-		const std::string body = req->get_path_parameter("path");
-		session->close(restbed::OK, body, { { "Content-Length", std::to_string(body.size()) } });
-	}
+        const std::string body = req->get_path_parameter("path");
+        session->close(restbed::OK, body, { { "Content-Length", std::to_string(body.size()) } });
+    }
 }
 
 using namespace BeatMods;
 
 int main()
 {
-	auto resc = std::make_shared<restbed::Resource>();
-	resc->set_path("{path: .*}");
-	resc->set_method_handler("GET", GET_root_handler);
+    auto resc = std::make_shared<restbed::Resource>();
+    resc->set_path("{path: .*}");
+    resc->set_method_handler("GET", GET_root_handler);
 
-	auto settings = std::make_shared<restbed::Settings>();
-	settings->set_port(7000);
-	settings->set_default_header("Connection", "close");
+    auto settings = std::make_shared<restbed::Settings>();
+    settings->set_port(7000);
+    settings->set_default_header("Connection", "close");
 
-	auto logger1 = StdoutLogger::New();
+    auto logger1 = StdoutLogger::New();
 
-	restbed::Service srv;
-	srv.publish(resc);
-	srv.set_logger(MultiLogger::New(logger1));
-	srv.start(settings);
+    restbed::Service srv;
+    srv.publish(resc);
+    srv.set_logger(MultiLogger::New(logger1));
+    srv.start(settings);
 
-	return 0;
+    return 0;
 }
