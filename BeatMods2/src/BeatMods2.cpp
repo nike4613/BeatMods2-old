98761deb703bd624e793407fbab1c8e44dda4309
Raphaël Thériault <raphael_theriault@outlook.com>
2019-06-15 20:07:59 -0400

    Multithreading test code

--- BeatMods2/src/BeatMods2.cpp
+++ BeatMods2/src/BeatMods2.cpp
@@ -5,6 +5,7 @@
 #include <restbed>
 #include <string>
 #include <memory>
+#include <thread>
 #include <cstdlib>
 #include <pqxx/pqxx>
 
@@ -15,7 +16,10 @@ namespace {
     {
         const auto& req = session->get_request();
 
-        const std::string body = req->get_path_parameter("path");
+        std::stringstream ss;
+        ss << req->get_path_parameter("path") << " - thread " << std::this_thread::get_id();
+
+        const std::string body = ss.str();
         session->close(restbed::OK, body, { { "Content-Length", std::to_string(body.size()) } });
     }
 }
@@ -28,8 +32,11 @@ int main()
     resc->set_path("{path: .*}");
     resc->set_method_handler("GET", GET_root_handler);
 
+    unsigned int worker_limit = std::thread::hardware_concurrency() / 4 * 3;
+
     auto settings = std::make_shared<restbed::Settings>();
     settings->set_port(7000);
+    settings->set_worker_limit(worker_limit);
     settings->set_default_header("Connection", "close");
 
     auto logger1 = StdoutLogger::New();
