94910b5fa654276b50de595efd22a8fd88ca117a
Raphaël Thériault <raphael_theriault@outlook.com>
2019-06-15 20:44:13 -0400

    Basic stress test script

---
+++ stress.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -vs "http://localhost:7000/[0-999]" &
+curl -vs "http://localhost:7000/[0-999]" &
+curl -vs "http://localhost:7000/[0-999]" &
+curl -vs "http://localhost:7000/[0-999]" &
+curl -vs "http://localhost:7000/[0-999]" &
+curl -vs "http://localhost:7000/[0-999]" &
+curl -vs "http://localhost:7000/[0-999]" &
+curl -vs "http://localhost:7000/[0-999]" &
+curl -vs "http://localhost:7000/[0-999]" &
+curl -vs "http://localhost:7000/[0-999]" &
+
+wait
