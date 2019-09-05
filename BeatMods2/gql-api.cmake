0a89dcee7ded5ef6ff6bf1c320a6735377528044
nike4613 <aaron.cirr.com@gmail.com>
2019-09-05 13:25:25 -0500

    Switched GraphQLGen to using the non-virtual generation (along with a fork to do that)

--- BeatMods2/gql-api.cmake
+++ BeatMods2/gql-api.cmake
@@ -6,7 +6,7 @@ add_custom_command(
     OUTPUT "generated/Schema.cpp"
     
     COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_CURRENT_BINARY_DIR}/generated/"
-    COMMAND ${GraphQLGen_BINARY} -s "${CMAKE_CURRENT_SOURCE_DIR}/api.graphql" -p "${CMAKE_CURRENT_BINARY_DIR}/generated/" -n BeatMods --no-stubs -v
+    COMMAND ${GraphQLGen_BINARY} -s "${CMAKE_CURRENT_SOURCE_DIR}/api.graphql" -p "${CMAKE_CURRENT_BINARY_DIR}/generated/" -n BeatMods --no-stubs --no-virtual -v
 
     DEPENDS GraphQLGen "${CMAKE_CURRENT_SOURCE_DIR}/api.graphql"
 )
